40bb475081f64b5f38e7e9cf00913b3dc035fd2b
James Thorne <james@thorne.bz>
2012-07-30 19:00:48 +0100

    Added MDB for Model

--- application/support/database.php
+++ application/support/database.php
@@ -16,3 +16,20 @@ class DB extends Controller
 	}
 }
 
+
+
+class MDB extends Model
+{
+	protected $dbh;
+	function __construct()
+	{
+		if(isset($_SESSION['username']) and isset($_SESSION['password']))
+		{
+			$this->dbh = new PDO("mysql:host={$_SESSION['server']}",$_SESSION['username'],$_SESSION['password']);	
+		}
+		
+		parent::__construct();
+		
+	}
+}
+
