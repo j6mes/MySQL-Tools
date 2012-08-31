e0fc3e57e1ddc82d9de526d9bfb54b8b37cbd17f
James Thorne <james@thorne.bz>
2012-08-31 22:17:57 +0100

    starting to redo documentation

--- application/configuration/servers.php
+++ application/configuration/servers.php
@@ -1,9 +1,25 @@
 <?php 
+/**
+ * Server Config Class
+ * Stores a white list of servers that the client can connect to.
+ *  
+ * @package default
+ * @author	James Thorne / MySQL Tools / Redslide
+ * @version 31/08/2012  
+ */
 
 class ServerConfig
 {
-	function getServers()
+	/**
+	 * getServers 
+	 * Returns an array of servers
+	 *
+	 * @return  array of server host names /ip addresses 
+	 * @author	James Thorne / MySQL Tools / Redslide
+	 * @version 31/08/2012  
+	 */
+	public function getServers()
 	{
-		return array ("127.0.0.1","109.104.92.173");
+		return array ("127.0.0.1");
 	}
 }
