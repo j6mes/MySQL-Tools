2562aae7474be5f2e689cbf37aa866f671244026
James Thorne <james@thorne.bz>
2012-10-30 18:17:06 +0000

    Added GPL message to index.php

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
+/**
+ * Copyright (C) 2012  James Thorne ~ www.mysqltools.org
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 
 ini_set("display_errors",1);
 ini_set("log_errors",1);
 ini_set('memory_limit','256M');
-
+ini_set('error_reporting' ,E_PARSE);
 ob_start();
 
 //load all the shizzz we need			TODO:Autoload the support folder.
