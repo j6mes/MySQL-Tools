093b370e0aa96adbf048171bf60d5287e5ddd611
James Thorne <james@thorne.bz>
2012-10-30 18:16:49 +0000

    Removed old dev server from the servers config

--- application/configuration/servers.php
+++ application/configuration/servers.php
@@ -1,22 +1,18 @@
 <?php 
 /**
  * Server Config Class
- * Stores a white list of servers that the client can connect to.
+ * Stores a whitelist of MySQL servers that the client can connect to.
  *  
  * @package default
  * @author	James Thorne / MySQL Tools / Redslide
  * @version 31/08/2012  
  */
-
 class ServerConfig
 {
 	/**
-	 * getServers 
 	 * Returns an array of servers
 	 *
 	 * @return  array of server host names /ip addresses 
-	 * @author	James Thorne / MySQL Tools / Redslide
-	 * @version 31/08/2012  
 	 */
 	public function getServers()
 	{
