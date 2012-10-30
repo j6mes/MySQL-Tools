6ed62fd7384f7d975b9413fbbb08113dba32a9ca
James Thorne <james@thorne.bz>
2012-10-30 18:47:10 +0000

    added no servers message

---
+++ application/fragments/login/noservers.php
@@ -0,0 +1,7 @@
+<div class="error>There are no servers in the configuration file.
+	<div>Please edit the following file
+		<pre>
+			/applicaiton/configuration/servers.php
+		</pre>
+	</div>	
+</div> 
